2a03d95d06cb801c9260294fef0024548ca44856
Amir Dehghan <amirmd76@gmail.com>
2019-07-16 18:31:39 +0430

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+readme = "Token Generator"
+
+setup(
+    name='taken',
+    version='1.0.0',
+    description='Random token generator',
+    long_description=readme,
+    author='AmirMohammad Dehghan',
+    author_email='dehghan@mit.edu',
+    url='https://github.com/amirmd76/taken.git',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
